b65bd5269ad17e84f33b12911d8998edbbb35523
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-04-09 10:15:17 -0400

    lab completed

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,13 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def toggle_active
+    if self.active
+      self.active = false
+    else
+      self.active = true
+    end
+    self.save
+  end
 end
